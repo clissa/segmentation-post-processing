7a8d121b8118dcb24849554a6c12a85061491084
clissa <clissa.professional@gmail.com>
2023-03-03 16:17:51 +0100

    Add utils.py script with plotting utils.
    Useful functions:
     - `plot_heatmap`: plot heatmap with colorbar
     - `plot_mask`: plot mask with a different color per object
     - `plot_masks_comparison`: plot masks side by side to compare before and after a processing step
     - `print_stats`: print some useful image stats

---
+++ src/utils.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import cm
+from matplotlib.axes._subplots import Axes
+from matplotlib.colors import ListedColormap
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+DATA_PATH = Path().cwd() / 'data'
+
+
+def plot_heatmap(heatmap: np.array, title: str, show: bool = True):
+    """Plot a heatmap with colorbar and title"""
+    fig, axis = plt.subplots(1, 1, figsize=(8, 8))
+    im = axis.pcolormesh(np.flipud(heatmap), cmap='jet', )
+    axis.set_title(title)
+    axis.set_aspect('equal')
+
+    # colorbar
+    divider = make_axes_locatable(axis)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(im, cax=cax)
+    if show:
+        plt.show()
+    return fig, axis, cax
+
+
+def plot_mask(mask: np.array, nobjs: int, title: str, show: bool = True):
+    """Plot mask with a different color per object and title."""
+    # create colormap: one color per object + add black for background
+    cmap = cm.get_cmap('tab20b', nobjs)
+    cmap = ListedColormap(np.insert(cmap.colors, 0, [0, 0, 0, 1], axis=0))
+
+    # plot mask
+    fig, axis = plt.subplots(1, 1, figsize=(8, 8))
+    axis.imshow(mask, cmap=cmap, vmin=0, vmax=nobjs)
+    axis.set_title(title)
+    if show:
+        plt.show()
+    return fig, axis, cmap
+
+
+def plot_masks_comparison(ax_raw: Axes, ax_processed: Axes, cmap: ListedColormap, show: bool = True):
+    """Plot masks before and after processing side by side for comparison."""
+    fig_cmp, ax_cmp = plt.subplots(1, 2, figsize=(14, 6))
+    ax_cmp[0].imshow(ax_raw.get_images()[0].get_array(), cmap=cmap)
+    ax_cmp[0].set_title('before')
+    ax_cmp[1].imshow(ax_processed.get_images()[0].get_array(), cmap=cmap)
+    ax_cmp[1].set_title('after')
+    plt.tight_layout()
+    if show:
+        plt.show()
+    return fig_cmp, ax_cmp
+
+
+def print_stats(image: np.array):
+    """Print useful image statistics."""
+    shape, dtype, min_val, max_val, n_uniques = image.shape, image.dtype, image.min(), image.max(), len(
+        np.unique(image))
+    print(f"{shape=};\t{dtype=};\t{min_val=};\t{max_val=};\t{n_uniques=}")
