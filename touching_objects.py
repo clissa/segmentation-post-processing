1f1e178ab39e6f816a5f68eb454c6a01a1167f5c
clissa <clissa.professional@gmail.com>
2023-03-10 12:24:20 +0100

    Refactor the size of the filter used as footprint as `fp_size` instead of `fs_size`

--- touching_objects.py
+++ touching_objects.py
@@ -19,7 +19,7 @@ parser.add_argument('fn', help="Filename of the heatmap to post-process. This as
 parser.add_argument('threshold', help="Binarization threshold.", type=float, default=0.5, nargs='?')
 parser.add_argument('min_size', help="Minimum allowed object size.", type=int, default=100, nargs='?')
 parser.add_argument('max_filt_size', help="Filter size used in maximum filter.", type=int, default=4, nargs='?')
-parser.add_argument('fs_size', help="Footprint size used in to find local maxima (`peak_local_max`).", type=int,
+parser.add_argument('fp_size', help="Footprint size used in to find local maxima (`peak_local_max`).", type=int,
                     default=6, nargs='?')
 args: Namespace = parser.parse_args()
 
@@ -47,9 +47,9 @@ def enhance_objects_basin(mask: np.array, max_filt_size: int, show: bool = True)
     return bool_mask, distance, maxi
 
 
-def get_basin_markers(maxi: np.array, fs_size: (int, int), bool_mask: np.array, show: bool = True, min_size: int = 0):
+def get_basin_markers(maxi: np.array, fp_size: (int, int), bool_mask: np.array, show: bool = True, min_size: int = 0):
     # get indexes of local maxima
-    local_maxi = peak_local_max(np.squeeze(maxi), indices=False, footprint=np.ones(fs_size), exclude_border=False,
+    local_maxi = peak_local_max(np.squeeze(maxi), indices=False, footprint=np.ones(fp_size), exclude_border=False,
                                 labels=np.squeeze(bool_mask))
     if min_size:
         local_maxi = remove_small_objects(local_maxi, min_size=min_size, connectivity=1)
@@ -63,19 +63,19 @@ def get_basin_markers(maxi: np.array, fs_size: (int, int), bool_mask: np.array, 
     print_stats(markers)
 
     if show:
-        plot_mask(local_maxi, f'local maximum; footprint={fs_size}')
+        plot_mask(local_maxi, f'local maximum; footprint={fp_size}')
 
     return local_maxi, markers, nmarkers
 
 
-def touching_objects(heatmap: np.array, th: float, min_size: int, max_filt_size: int, fs_size: (int, int),
+def touching_objects(heatmap: np.array, th: float, min_size: int, max_filt_size: int, fp_size: (int, int),
                      show: bool = True):
     # first get cleaned mask
     t_mask, cleaned_mask, thresholded_objects = small_objects(heatmap, th, min_size, show=False)
 
     # find local maxima; NOTE: play with `max_filter_size` and `footprint` parameters
     bool_mask, distance, maxi = enhance_objects_basin(cleaned_mask, max_filt_size, show=show)
-    local_maxi, markers, nmarkers = get_basin_markers(maxi, fs_size, bool_mask, show=show)
+    local_maxi, markers, nmarkers = get_basin_markers(maxi, fp_size, bool_mask, show=show)
 
     # finally apply watershed to separate close-by objects; NOTE: play with `compactness` and `watershed_line` parameters
     watershed_mask = watershed(-distance, markers, mask=np.squeeze(bool_mask), compactness=1, watershed_line=False)
@@ -87,7 +87,7 @@ def touching_objects(heatmap: np.array, th: float, min_size: int, max_filt_size:
     fig_watershed, ax_watershed, cmap_watershed = plot_mask(watershed_mask, "watershed")
     plot_masks_comparison(ax_clean, ax_watershed, cmap_watershed, title="Separe close/overlapping objects")
 
-    local_maxi_cleaned, markers_cleaned, nmarkers_cleaned = get_basin_markers(maxi, fs_size, bool_mask, min_size=10)
+    local_maxi_cleaned, markers_cleaned, nmarkers_cleaned = get_basin_markers(maxi, fp_size, bool_mask, min_size=10)
     watershed_cleaned_mask = watershed(-distance, markers_cleaned, mask=np.squeeze(bool_mask), compactness=1,
                                        watershed_line=False)
     fig_watershed_cleaned, ax_watershed_cleaned, cmap_watershed_cleaned = plot_mask(watershed_cleaned_mask,
@@ -102,9 +102,9 @@ if __name__ == '__main__':
     # get sample mask
     # fn: str = '11.png'
     heatmap: np.array = io.imread(DATA_PATH / args.fn, as_gray=True) / 255
-    fs_size = (args.fs_size, args.fs_size)
+    footprint_size = (args.fp_size, args.fp_size)
     watershed_mask, watershed_cleaned_mask = touching_objects(heatmap, args.threshold, args.min_size,
-                                                              args.max_filt_size, fs_size, show=False)
+                                                              args.max_filt_size, footprint_size, show=False)
 
     # save masks without small objects
     outpath = DATA_PATH / 'post-processed'
